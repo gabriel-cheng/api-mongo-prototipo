0a0a62a1f77831e84a3ad9b64a8ae69d5f906f49
Gabriel Cheng <gabriel_cheng.contato@outlook.com>
2022-11-02 19:53:56 -0300

    :recycle: refactor - correção do eslint :fire:

--- router/router.js
+++ router/router.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.send('Você está na home!');
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-const router = require('./router/router');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
